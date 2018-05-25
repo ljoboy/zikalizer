158939c2f5d3ec992599c4d026f57ff87e369348
Lijerbul LJOBOY <jonathanyombo@gmail.com>
2018-05-25 12:08:58 +0200

    One more add

--- installer/windows/create-installer.js
+++ installer/windows/create-installer.js
@@ -5,24 +5,24 @@ const createWindowsInstaller = require("electron-winstaller").createWindowsInsta
 const path = require("path");
 
 getInstallerConfig().then(createWindowsInstaller).catch((error) => {
-  console.error(error.message || error);
-  process.exit(1);
+    console.error(error.message || error);
+    process.exit(1);
 });
 
 function getInstallerConfig() {
-  console.log("Creating windows installer");
-  const rootPath = path.join("./");
-  const outPath = path.join(rootPath, "dist");
+    console.log("Creating windows installer");
+    const rootPath = path.join("./");
+    const outPath = path.join(rootPath, "dist");
 
-  return Promise.resolve({
-    appDirectory: path.join(outPath, "Media\ Player-win32-x64"),
-    authors: "Tim Nguyen",
-    noMsi: true,
-    outputDirectory: path.join(outPath, "windows-installer"),
-    iconUrl: "https://raw.githubusercontent.com/nt1m/media-player/gh-pages/img/icon.ico",
-    exe: "Media Player.exe",
-    setupExe: "MediaPlayerInstaller.exe",
-    setupIcon: path.join(rootPath, "img/icon.ico"),
-    skipUpdateIcon: true
-  });
+    return Promise.resolve({
+        appDirectory: path.join(outPath, "Media\ Player-win32-x64"),
+        authors: "Lijerbul LJOBOY",
+        noMsi: true,
+        outputDirectory: path.join(outPath, "windows-installer"),
+        iconUrl: "https://raw.githubusercontent.com/nt1m/media-player/gh-pages/img/icon.ico",
+        exe: "Zikalizer.exe",
+        setupExe: "ZikalizerInstaller.exe",
+        setupIcon: path.join(rootPath, "img/icon.ico"),
+        skipUpdateIcon: true
+    });
 }
